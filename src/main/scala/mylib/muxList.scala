39bf7a1e4e18bf1d082745861915fab1e3e5d4d5
yportne <yportne@163.com>
2022-06-23 23:53:54 +0800

    doc(muxList):show an example

--- src/main/scala/mylib/muxList.scala
+++ src/main/scala/mylib/muxList.scala
@@ -3,9 +3,29 @@ package mylib
 import spinal.core._
 import spinal.lib._
 
+/**
+  * in verilog you may need
+  * ```
+  * output <= a ? sigA :
+              b ? sigB :
+              c ? sigC :
+                  sigD
+  * ```
+  * use this function you can do like this:
+  * ```
+  * output := muxList(
+  *       (a, sigA),
+  *       (b, sigB),
+  *       (c, sigC),
+  *       (True, sigD)
+  * )
+  * ```
+  */
 object muxList {
   def apply[T <: Data](list: List[(Bool, T)]): T = {
-    val ret = list.reverse.drop(1).scanLeft(list.last._2)((b,a) => a._1?a._2|b)
-    ret.last
+    list.reverse.drop(1).scanLeft(list.last._2)((b,a) => a._1?a._2|b).last
+  }
+  def apply[T <: Data](list: (Bool, T) *): T = {
+    apply(list.toList)
   }
 }
